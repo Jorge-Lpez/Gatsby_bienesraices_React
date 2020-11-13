8f0d3f558cffbbd6b6e47796f6061465ae90021d
Abascal <jorgeabascal20@gmail.com>
2020-11-13 16:33:31 -0500

    Agregando formulario y estilos a los formularios

--- src/components/listadopropiedades.js
+++ src/components/listadopropiedades.js
@@ -3,12 +3,18 @@ import { css } from "@emotion/core";
 import UsePropiedades from "../hooks/usePropiedades";
 import PropiedadPreview from "../components/propiedadpreviwe";
 import ListadoCSS from "../css/listadopropiedades.module.css";
+import useFiltro  from "../hooks/useFiltro";
 
 const ListadoPropiedades = () => {
     const datos = UsePropiedades();
     //STATE que guarda los datos
     const [propiedades, guardarPropiedades] = useState([]);
     //Actualizando state al momento de cargar propiedades
+
+    //Filtrado de propiedades
+    const { busqueda, FlitroUI } = useFiltro();
+
+
     useEffect(() => {
         guardarPropiedades(datos);
     }, [])
@@ -20,6 +26,8 @@ const ListadoPropiedades = () => {
                     margin-top: 20px;
                 `}
             >Nuestras Propiedades</h2>
+            
+            {FlitroUI()}
 
             <ul className={ListadoCSS.propiedades}>
                 {propiedades.map( propiedad => (
